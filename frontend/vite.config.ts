3e612e1680a554a00fd9ef10c6b4d059e6ec688f
Corey Floyd <corey.floyd@smartthings.com>
2024-03-18 21:07:53 -0500

    more dependency fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@sanity": path.resolve(__dirname, "../studio/sanity"),
     },
   },
 });
